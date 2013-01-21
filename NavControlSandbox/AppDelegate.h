bbb76765d874b8cfb7e43d2a83015a995434cdfe
Raymond T. Hightower <rth@wisdomgroup.com>
2013-01-20 22:12:04 -0600

    Initial Commit

---
+++ NavControlSandbox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavControlSandbox
+//
+//  Created by RTH on 1/20/13.
+//  Copyright (c) 2013 WisdomGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
